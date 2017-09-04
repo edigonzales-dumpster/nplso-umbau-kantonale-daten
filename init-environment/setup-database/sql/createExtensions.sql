5267764a7eed1184c66c4640734084cc87dfbb58
Stefan Ziegler <edi.gonzales@gmail.com>
2017-09-04 20:56:18 +0200

    bln model for us

--- init-environment/setup-database/sql/createExtensions.sql
+++ init-environment/setup-database/sql/createExtensions.sql
@@ -1,2 +1,4 @@
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+--CREATE EXTENSION IF NOT EXISTS postgis;
+--CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+SELECT UpdateGeometrySRID('afu_gws', 'public_aww_gszoar','the_geom',2056);
